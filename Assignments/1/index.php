f8a868e7e49c708a441c74bfb54082ec09ef0c8b
jahenderson <jahenderson@csumb.edu>
2017-09-11 21:19:56 +0000

    Don't know...rebase

---
+++ Assignments/1/index.php
@@ -0,0 +1,1 @@
+In Assignment 1
--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-This is a working document i guess.
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <div>
+            <a href="/Assignments">Assignments</a>
+        </div>
+        <div>
+            <a href="/Labs">Labs</a>
+        </div>
+    </body>
+</html>
